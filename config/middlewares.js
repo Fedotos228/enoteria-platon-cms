5d0ce232d254c79a473c54666d6d70e906df4dc2
Fedotos228 <tudor.onceanu@gmail.com>
2024-06-17 11:11:15 +0300

    adding cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000']
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://enoteria-platon-8b572188cb73.herokuapp.com/']
     }
   },
   'strapi::poweredBy',
