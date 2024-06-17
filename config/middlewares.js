a4c309ec1edc658ede208c398f572653e541f419
Fedotos228 <tudor.onceanu@gmail.com>
2024-06-17 14:37:10 +0300

    mb last one

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://enoteria-platon-8b572188cb73.herokuapp.com/', 'https://enoteria-platon.vercel.app/']
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://enoteria-platon-8b572188cb73.herokuapp.com/', 'https://enoteria-platon.vercel.app']
     }
   },
   'strapi::poweredBy',
