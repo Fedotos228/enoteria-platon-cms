727ceedac82eb418f34b5c5db6bfdd515a2003e0
Fedotos228 <tudor.onceanu@gmail.com>
2024-06-17 11:59:09 +0300

    adding front-end domen

--- config/env/production/admin.js
+++ config/env/production/admin.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-  url: "/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
+  url: "http://localhost:3000/", // Note: The administration will be accessible from the root of the domain (ex: http://yourfrontend.com/)
   serveAdminPanel: false, // http://yourbackend.com will not serve any static admin files
 })
