4701a6a90d17565edde65da748d644ddd5377f25
Fedotos228 <tudor.onceanu@gmail.com>
2024-07-05 11:53:10 +0300

    adding cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   slugify: {
     enabled: true,
     config: {
@@ -23,6 +23,20 @@ module.exports = () => ({
           field: 'slug',
           references: 'title',
         }
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
   },
